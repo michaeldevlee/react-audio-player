a24722ff9d92d04ab703fffc91675b885559a4a1
mikelee53 <58196525+mikelee53@users.noreply.github.com>
2022-05-09 22:18:58 -0700

    corrected song paths

--- src/App.js
+++ src/App.js
@@ -9,36 +9,36 @@ const [songs] = useState([
   {
     "title": "Get Down",
     "artist": "IMLC",
-    "img_src" : "./react-audio-player/Songs_Images/Independent.png",
-    "src" : "./react-audio-player/Songs/IMLC-Get-Down.mp3",
+    "img_src" : "./Songs_Images/Independent.png",
+    "src" : "./Songs/IMLC-Get-Down.mp3",
     "link" : "https://freemusicarchive.org/music/independent-music-licensing-collective-imlc/new-launch-by-ketsa/get-down/"
   },
   {
     "title": "Warm Feeling",
     "artist": "AHOAMI",
-    "img_src" : "./react-audio-player/Songs_Images/waves.jpg",
-    "src" : "./react-audio-player/Songs/warm-feeling_by_ahoami.mp3",
+    "img_src" : "./Songs_Images/waves.jpg",
+    "src" : ". /Songs/warm-feeling_by_ahoami.mp3",
     "link" : " Music from Tunetank.com AHOAMI - Warm Feeling (Copyright Free Music) Download free: https://tunetank.com/track/3499-warm-feeling"
   },
   {
     "title": "Dreaming of Water",
     "artist": "IMLC",
-    "img_src" : "./react-audio-player/Songs_Images/Independent.png",
-    "src" : "./react-audio-player/Songs/IMLC-Dreaming-of-Water.mp3",
+    "img_src" : "./Songs_Images/Independent.png",
+    "src" : ". /Songs/IMLC-Dreaming-of-Water.mp3",
     "link" : " https://freemusicarchive.org/music/independent-music-licensing-collective-imlc/new-launch-by-ketsa/dreaming-of-water/"
   },
   {
     "title": "Once Upon A Time",
     "artist": "Maarten Schellekens",
-    "img_src" : "./react-audio-player/Songs_Images/Once Upon A Time.png",
-    "src" : "./react-audio-player/Songs/Maarten-Schellekens-Once-Upon-A-Time.mp3",
+    "img_src" : "./Songs_Images/Once Upon A Time.png",
+    "src" : ". /Songs/Maarten-Schellekens-Once-Upon-A-Time.mp3",
     "link" : " https://freemusicarchive.org/music/maarten-schellekens/orchestral-works/once-upon-a-time/"
   },
   {
     "title": "Whatever",
     "artist": "HoliznaCC0",
-    "img_src" : "./react-audio-player/Songs_Images/Whatever.png",
-    "src" : "./react-audio-player/Songs/HoliznaCC0-Whatever.mp3",
+    "img_src" : "./Songs_Images/Whatever.png",
+    "src" : "./Songs/HoliznaCC0-Whatever.mp3",
     "link" : "https://freemusicarchive.org/music/holiznacc0/lo-fi-and-chill/whatever-2/"
   },
   
